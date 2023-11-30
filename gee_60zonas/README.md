a4773b399d2812d0f43959f26e12dbbb498662bf
Juan Manuel Barreneche <jumanbar@gmail.com>
2023-11-30 11:00:07 -0300

    scripts bash loops

---
+++ gee_60zonas/README.md
@@ -0,0 +1,12 @@
+
+Ejecutar comando para un día
+
+```sh
+python3 main.py --id-zona 1 --end-date 2023-11-25
+```
+
+Ejecutar entre dos fechas:
+
+```sh
+./run_absent_days.sh 2023-11-15 2023-11-17
+```
--- gee_60zonas/run_day_60zones.sh
+++ gee_60zonas/run_day_60zones.sh
@@ -10,10 +10,8 @@ function pad () {
 }
 
 # FECHAS:
-ini_date=$(date -d $1 "+%Y-%m-%d")
-fin_date=$(date -d $2 "+%Y-%m-%d")
+fin_date=$(date -d $1 "+%Y-%m-%d")
 
-echo "FECHA INICIAL: $ini_date"
 echo "FECHA FINAL: $fin_date"
 # echo "FECHA INICIAL NUM: $(date -d ${ini_date} +%s)"
 # echo "FECHA FINAL NUM: $(date -d ${fin_date} +%s)"
@@ -21,13 +19,14 @@ echo "FECHA FINAL: $fin_date"
 
 for j in {1..60};
 do
-	DATE=$ini_date
-	while [ $(date -d ${DATE} +%s) -le $(date -d ${fin_date} +%s) ]
-	do
-		echo "id_zona: $(pad $j) / 60 | FECHA: $DATE / $fin_date"
-		python3 main.py --id-zona $(pad $j) --end-date $DATE > log/log-z-$(pad $j)-$DATE.log
-		DATE=$(date -d "${DATE} +1 days" "+%Y-%m-%d")
-	done;
+	DATE=$fin_date
+    echo "id_zona: $(pad $j) / 60 | FECHA: $DATE"
+	python3 main.py --id-zona $(pad $j) --end-date $DATE > log/log-z-$(pad $j)-$DATE.log
+	# while [ $(date -d ${DATE} +%s) -le $(date -d ${fin_date} +%s) ]
+	# do
+
+	# 	DATE=$(date -d "${DATE} +1 days" "+%Y-%m-%d")
+	# done;
 done;
 
 # if [ $A -lt 10 ] ; then echo "0$A"; fi
