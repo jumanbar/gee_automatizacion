289004261bd64c9c9e56244da3a8061bac4e708d
Juan Manuel Barreneche <jumanbar@gmail.com>
2024-08-02 22:00:17 -0300

    Update tests.R
    El útimo gráfico, con separación de 3 fasces muy linda

--- tests.R
+++ tests.R
@@ -4,7 +4,6 @@ m <- jsonlite::read_json("~/gee_automatizacion/muestra.json", simplifyVector = T
   as_tibble() %>%
   mutate(p = row_number())
 
-
 m %>%
   ggplot() +
   aes(p, nd_date) +
@@ -39,7 +38,7 @@ library(colorSpec)
 ?luminsivity.1nm$spectra[,1:4]
 lef <- luminsivity.1nm$spectra[4,]
 
-# Bandas Rojo Verde y Azul (en sentinel-2: https://sentiwiki.copernicus.eu/web/s2-mission; lambda = )
+# Bandas Rojo Verde y Azul (en sentinel-2: https://sentiwiki.copernicus.eu/web/s2-mission)
 B2 <- lef['490'] # Azul
 B3 <- lef['560'] # Verde
 B4 <- lef['665'] # Rojo
@@ -47,3 +46,33 @@ c(B2, B3, B4)
 
 
 
+m <- jsonlite::read_json("~/gee_automatizacion/muestra_test.json", simplifyVector = TRUE) %>%
+  as_tibble() %>%
+  mutate(p = row_number())
+
+m %>%
+  ggplot() +
+  aes(p, ndwi) +
+  geom_col()
+
+m %>%
+  ggplot() +
+  aes(p, ndbr) +
+  geom_step()
+
+m %>%
+  ggplot() +
+  aes(p, ndbg) +
+  geom_step()
+
+ml <- m %>%
+  pivot_longer(cols=-p, names_to = 'Band', values_to = 'Rrs')
+
+ml %>%
+  ggplot() +
+  aes(p, Rrs) +
+  geom_point(shape = 21, alpha = 0.8, fill = "darkgrey") +
+  facet_wrap(vars(Band))
+  # facet_wrap(vars(Band), scales = "free_y")
+
+ggsave("acum_indices_agua.png", scale = 0.9, width = 20, height = 15, units = "cm")
